9504bef71cd8dbd57d456f33d7f575854533a814
matthewsciascia <100806521+matthewsciascia@users.noreply.github.com>
2022-03-02 17:00:00 -0500

    Update test.js

--- test.js
+++ test.js
@@ -10,3 +10,18 @@ console.log(cFE.getFinalGrade(20, 80));
 console.log(cFLG.getFinalLetterGrade(92));
 //first: Hw final hw grade (0 to 20) 
 //second: Exam final grade (0 to 80)
+console.log(cHW.computeHW(-10, 100, 100));//test negatives
+console.log(cEG.computeExams(-5, 100));//test negatives
+console.log(cFE.getFinalGrade(-4, 80));//test negatives
+console.log(cFLG.getFinalLetterGrade(92));
+
+console.log(cHW.computeHW(100, 105, 100));//test over 100%
+console.log(cEG.computeExams(200, 100));//test over 100%
+console.log(cFE.getFinalGrade(20, 80));//test over 100%
+console.log(cFLG.getFinalLetterGrade(92));
+
+//test boundaries
+console.log(cHW.computeHW(0, 150, 100));
+console.log(cEG.computeExams(0, 100));
+console.log(cFE.getFinalGrade(0, 80));
+console.log(cFLG.getFinalLetterGrade(92));
